3372348c0777698d98d2adbe76df357e3b103e45
Janetta Kataoeva <jkattoeva@gmail.com>
2021-12-24 14:42:42 +0600

    learn else if

--- script.js
+++ script.js
@@ -23,4 +23,13 @@
 // var year = 2020 - age;
 // console.log(year);
 
-alert();
+var year = prompt("What year Kyrgyzstan gained independence?");
+if (year > 1991) {
+  alert("This year is after independence year.");
+}
+else if (year === "1991") {
+  alert("You got it!");
+}
+else {
+  alert("This year is before independence year.");
+}
