44dd138bf57b96a487cbca58451afc6fe0c1fa8c
Janetta Kataoeva <jkattoeva@gmail.com>
2021-12-24 15:40:14 +0600

    practice for , if , else ,while

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 // }
 // alert(message);
 
-
 // var year = prompt("What year Kyrgyzstan gained independence?");
 // if (year === "1991") {
 //   alert("Correct!");
@@ -14,7 +13,7 @@
 // }
 
 // var example1 = "200" + 200; //NaN - not a number
-// var example2 = "10" + "10"; 
+// var example2 = "10" + "10";
 
 // console.log(example1);
 // console.log(example2);
@@ -49,23 +48,45 @@
 //    console.log(currentNumber);
 //  }
 
-//  for (var i=0; i<10; i++)  { 
+//  for (var i=0; i<10; i++)  {
 //    console.log(i);
 //  }
 
-var city = prompt("What is the country you want know capital of?");
-if (country === "USA") {
-  alert("Washington");
-}
-else if (country === "Kyrgyzstan") {
-  alert("Bishkek");
-}
-else if (country === "Kazahstan") {
-  alert("Nur Sultan");
-}
-else {
-  alert("We don't know that country");
-}
+// var city = prompt("What is the country you want know capital of?");
+// if (country === "USA") {
+//   alert("Washington");
+// }
+// else if (country === "Kyrgyzstan") {
+//   alert("Bishkek");
+// }
+// else if (country === "Kazahstan") {
+//   alert("Nur Sultan");
+// }
+// else {
+//   alert("We don't know that country");
+// }
 
+// for (var i = 2; i < 150; i += 2) {
+//   console.log(i);
+// }
+// for (var i = 1; i < 150; i += 3) {
+//   console.log(i);
+// }
 
+// var currentNumber = 1;
+// var stopNumber = 150;
 
+// while (currentNumber < stopNumber) {
+//   //пока
+//   currentNumber = currentNumber + 4;
+//   console.log(currentNumber);
+// }
+
+// for (var i = 0; i < 100; i +=2) {
+//   console.log(i);
+// }
+
+for (var i = 0; i < 100; i++) {
+  if( i % 2 === 0 );
+  console.log(i);
+}
