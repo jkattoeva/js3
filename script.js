19026c06b509c6a88be5a96a0ed2e462d84848e3
Janetta Kataoeva <jkattoeva@gmail.com>
2021-12-24 14:27:43 +0600

    added examples

---
+++ script.js
@@ -0,0 +1,21 @@
+// var message = prompt("What is your message?");
+// if (message === "") {
+//   message = "no message";
+// }
+// alert(message);
+
+
+// var year = prompt("What year Kyrgyzstan gained independence?");
+// if (year === "1991") {
+//   alert("Correct!");
+// }
+// else {
+//   alert("Incorrect!");
+// }
+
+// var example1 = "200" + 200; //NaN - not a number
+// var example2 = "10" + "10"; 
+
+// console.log(example1);
+// console.log(example2);
+
